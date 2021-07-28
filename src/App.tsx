88b6ec8a455029c77aa103199e7ac26a7dfc931c
MiDrAjX <jeisonmarques78@gmail.com>
2021-07-27 22:08:40 -0300

    Começando o Projeto

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import Welcome from './pages/Welcome';
+ 
+export default function App(){
+   return(
+    <Welcome/>
+       
+   )
+ }
