8a17a854c2ba481bde7b1940081d129cfc8e791c
MiDrAjX <jeisonmarques78@gmail.com>
2021-07-29 21:07:36 -0300

    Construindo a lista de Personagens

---
+++ src/pages/CharacterList/index.tsx
@@ -0,0 +1,112 @@
+import React, {useState, useEffect} from 'react';
+import {useNavigation} from '@react-navigation/core';
+import {FlatList , ActivityIndicator, View, Text, ImageSourcePropType, Image } from 'react-native';
+import api from "../../services/api";
+import { Load } from '../../components/Load';
+import colors from '../../styles/colors';
+import  Card  from '../../components/Card';
+
+import {
+  CardContainer,
+  CardImage,
+  CardInformation,
+  CardTitle,
+  CardSubTitle,
+  CardText,
+  CardButtonIcon,
+} from '../../components/Card/styles';
+import { Container, Header, Title, Quantidade } from './styles';
+import { SearchField } from '../../components/SearchField';
+
+
+ export interface Character{
+  id: string;
+  name: string;
+  image: string;
+  species: string;
+  origin:{
+    name:string,
+    url:string
+  }
+  }
+
+
+export default function CharacterList(){
+
+  const [characters, setCharacters]= useState<Character[]>([]);
+  const [charactersList, setCharactersList]= useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const [page, setPage]= useState(1);
+  const [loadingMore, setLoadingMore] = useState(false);
+  const [loadedAll, setLoadedAll] = useState(false)
+
+  
+  const navigation = useNavigation()
+  function handleCharacterListAcess(){
+    navigation.navigate("CharacterOverView")
+}
+
+  function handleFetchMore(){
+    setLoadingMore(true);
+    setPage(oldValue=> oldValue+1);
+    
+    fetchCharacters();
+}
+  async function fetchCharacters(){
+    
+    const {data} = await api.get(`/character/?page=${page}`)
+     
+      if(!data){
+          return setLoading(true);
+      }
+      if(page>1){
+          setCharacters(oldValue => [...oldValue, ...data.results]);
+          setCharactersList(oldValue => [...oldValue, ...data.results]);
+      }else{
+          
+          setCharacters(data.results);
+
+          setCharactersList(data.results);
+      }
+      setLoading(false);
+      setLoadingMore(false);
+  }
+
+
+ useEffect(() => {
+  fetchCharacters()
+  },[])
+ 
+
+  if(loading){ 
+      return <Load/>
+  }
+  return(
+  <Container>
+    <Header>
+      <Title>Listagem</Title>
+      <Quantidade>Muitos Personagens</Quantidade>
+    </Header>
+    <View>
+      <SearchField/>
+    </View>
+
+    
+    <FlatList
+      data={characters}
+      keyExtractor={Character => Character.id}
+      renderItem={({item})=>(
+        <Card item={item}/>
+      )}
+      showsVerticalScrollIndicator={false}
+          onEndReachedThreshold={0.1}
+          onEndReached={()=>handleFetchMore()}
+          ListFooterComponent={
+              loadingMore ? <ActivityIndicator color={colors.blue_dark}/>:<></>
+            }
+    />  
+       
+  </Container>
+  )
+  }
---
+++ src/pages/CharacterList/styles.ts
@@ -0,0 +1,42 @@
+import styled from 'styled-components/native';
+
+
+export const Container = styled.View`
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  position: relative;
+  background-color: #C4C4C4;
+  ` 
+
+export const Header = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 125px;
+  padding: 0 20px;
+  background: #1E2047;
+  `;
+
+  export const Title = styled.Text`
+ 
+  color: #FFFFFF;
+  font-family: Poppins;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 32px;
+  `
+
+  export const Quantidade = styled.Text`
+  color: #C1C1C1;
+  font-family: Poppins;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 32px;
+  `;
+
+
+
