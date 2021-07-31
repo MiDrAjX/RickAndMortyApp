e9e36282054cacf5fe1f9242e11dca4ec5951389
MiDrAjX <jeisonmarques78@gmail.com>
2021-07-30 22:56:48 -0300

    Estilizando, Função retorno, Pesquisa no Google

--- src/pages/CharacterOverView/index.tsx
+++ src/pages/CharacterOverView/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
+import React, { useCallback, useEffect, useState } from 'react';
+import api from "../../services/api";
+import { Linking, Text, View } from 'react-native';
 
 import { 
   Container, 
@@ -9,29 +11,107 @@ import {
   ViewRow, 
   CharacterSubTitle, 
   CharacterText,
+  ViewSpace,
+  CharacterLocation,
+  ViewSpace2,
+  Footer,
+  FooterText,
+  ReturnButton,
+  ReturnButtonIcon,
+  CharacterInfo,
 } from './styles';
+import { Load } from '../../components/Load';
+
+type ParamList = {
+  CharacterOverView: {
+    id: string;
+  };
+};
+export interface Character{
+  id: string;
+  name: string;
+  status:string;
+  gender:string;
+  image: string;
+  species: string;
+  origin:{
+    name:string,
+    url:string
+  }
+  location:{
+    name:string,
+    url:string
+  }
+  }
 
 export default function CharacterOverView(){
+  const [character, setCharacter] = useState<Character>();
+  const [loading, setLoading] = useState(true);
 
+  const route = useRoute<RouteProp<ParamList, 'CharacterOverView'>>();
+  const { id } = route.params;
+
+  const navigation = useNavigation();
+  function handleReturn(){
+    navigation.navigate("CharacterList")
+}
+  useEffect(() => {
+    api.get(`/character/${id}`).then((response) => {
+      if(!response){
+        return setLoading(true);}
+      setCharacter(response.data);
+      setLoading(false);
+    });
+  }, []);
+  const handleGoogle = useCallback(async searchName => {
+    Linking.openURL(`https://www.google.com/search?q=${searchName}+rick+and+morty`);
+  }, []);
+
+  if(loading){ 
+    return <Load/>
+}
   return(
    <Container>
-     <CharacterImage source={{ uri: "https://rickandmortyapi.com/api/character/avatar/61.jpeg" }} resizeMode="cover"/>
+     
+     <CharacterImage source={{ uri: character.image }} resizeMode="cover"/>
+     <ReturnButton onPress={handleReturn}>
+        <ReturnButtonIcon name="arrow-left" />
+      </ReturnButton>
+    <CharacterInfo>
      <ViewRow>
      <CharacterTitle numberOfLines={1}>
-        Morty
+     {character.name}
       </CharacterTitle>
       <CharacterButtonIcon name="heart"/>
      </ViewRow>
      <ViewRow>
-     <View>
+     <ViewSpace>
         <CharacterSubTitle>Species:</CharacterSubTitle>
-        <CharacterText>Human</CharacterText>
-      </View>
-      <View>
+        <CharacterText>{character.species}</CharacterText>
+      </ViewSpace>
+      <ViewSpace>
         <CharacterSubTitle>Gender:</CharacterSubTitle>
-        <CharacterText>Male</CharacterText>
-      </View>
+        <CharacterText>{character.gender}</CharacterText>
+      </ViewSpace>
       </ViewRow>
+      <ViewSpace>
+        <CharacterLocation>Location:</CharacterLocation>
+        <CharacterLocation>{character.location.name}</CharacterLocation>
+      </ViewSpace>
+      <ViewRow>
+     <ViewSpace2 >
+        <CharacterSubTitle >Origin:</CharacterSubTitle>
+        <CharacterText>{character.origin.name}</CharacterText>
+      </ViewSpace2>
+      <ViewSpace2>
+        <CharacterSubTitle>Status:</CharacterSubTitle>
+        <CharacterText>{character.status}</CharacterText>
+      </ViewSpace2>
+      </ViewRow>
+      </CharacterInfo>
+      <Footer onPress={()=>handleGoogle(character.name)}>
+        <FooterText>Buscar no Google</FooterText>
+      </Footer>
    </Container>
   )
   }
--- src/pages/CharacterOverView/styles.ts
+++ src/pages/CharacterOverView/styles.ts
@@ -1,17 +1,35 @@
 import styled from 'styled-components/native';
 import Icon from 'react-native-vector-icons/Feather';
 
+export const Container = styled.View`
+  flex:1;
+  width: 100%;
+  height: 100%;
+  
+  background-color: #F0F0F5;
+  ` 
+
 export const CharacterImage = styled.Image`
-margin-bottom: 31px;
-width: 411px;
+margin-bottom: 11px;
+width: 100%;
 height: 437px;
 left: 0px;
 top: 0px;
 `;
+export const CharacterInfo = styled.View`
+  flex: 1;
+  justify-content: space-between;
+  margin-top: -30px;
+  margin-bottom: 56px;
+  padding: 15px 25px;
+  background: #F0F0F5;
+`;
+
 
 export const CharacterTitle = styled.Text`
 margin-bottom: 30px;
-left: -55px;
+left: 0px;
+position:absolute;
 font-family: Poppins;
 font-style: normal;
 font-weight: 600;
@@ -22,13 +40,14 @@ color: #333333;
 `;
 
 export const CharacterButtonIcon = styled(Icon)`
-left: 55px;
+left: 320px;
+position:absolute;
 padding-top: 3px;
 color: #1E2047;
 font-size: 30px;
 `;
 export const CharacterSubTitle = styled.Text`
-left: -50px;
+width: 100%;
 font-family: Poppins;
 font-style: normal;
 font-weight: 600;
@@ -38,7 +57,18 @@ color: #333333;
 `;
 
 export const CharacterText = styled.Text`
-left: -50px;
+font-family: Poppins;
+font-style: normal;
+font-weight: 500;
+font-size: 16px;
+line-height: 24px;
+
+color: #333333;
+`;
+
+export const CharacterLocation = styled.Text`
+left: 58px;
+top:-10px;
 font-family: Poppins;
 font-style: normal;
 font-weight: 500;
@@ -49,17 +79,41 @@ color: #333333;
 `;
 
 export const ViewRow= styled.View`
+
 flex-direction:row;
 justify-content:space-around;
 `
+export const ViewSpace= styled.View`
+left:-55px;
+top:10px;
+`
+export const ViewSpace2= styled.View`
+width: 100%;
+left: 90px;
 
-export const Container = styled.View`
-  
-  width: 100%;
-  height: 100%;
-  position: relative;
-  background-color: #F0F0F5;
-  ` 
+`
+
+export const Footer = styled.TouchableOpacity`
+position: absolute;
+left: 0;
+bottom: 0;
+right: 0;
+flex-direction: row;
+align-items: center;
+justify-content: center;
+padding: 10px 0;
+height: 56px;
+background-color: #1E2047;
+` 
+export const FooterText= styled.Text`
+margin-left: 16px;
+font-size: 20px;
+font-family: Poppins;
+line-height: 33px;
+text-align: center;
+
+color: #E1E1E6;
+`
   export const Title = styled.Text`
  
   color: #FFFFFF;
@@ -69,5 +123,23 @@ export const Container = styled.View`
   font-size: 20px;
   line-height: 32px;
   `
+  export const ReturnButton = styled.TouchableOpacity`
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  z-index: 999;
+  top: 15px;
+  left: 15px;
+  height: 50px;
+  width: 50px;
+  background: #1E2047;
+  border-radius: 25px;
+`;
+
+export const ReturnButtonIcon = styled(Icon)`
+  margin-left: 3px;
+  color: #FFFFFF;
+  font-size: 26px;
+`;
 
 
